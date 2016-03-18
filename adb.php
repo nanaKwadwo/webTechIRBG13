ae07d13fc3c9df152b2ef6053257dbbe8b42aaab
william <wkocran@gmail.com>
2016-03-18 15:32:23 +0000

    adb file respnsible for querying, fetching, connecting

---
+++ adb.php
@@ -0,0 +1,91 @@
+<?php
+/**
+*Database connection helper
+*/
+include_once("setting.php");
+/**
+* Database connection helper class
+*/
+class adb{
+	var $db=null;
+	var $result=null;
+	function adb(){
+	}
+	/**
+	*Connect to database 
+	*@return boolean ture if connected else false
+	*/
+	function connect(){
+		
+		//connect
+		$this->db=new mysqli(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
+		if($this->db->connect_errno){
+			//no connection, exit
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	*Query the database 
+	*@param string $strQuery sql string to execute
+	*/
+	function query($strQuery){
+		if(!$this->connect()){
+			return false;
+		}
+		if($this->db==null){
+			return false;
+		}
+		$this->result=$this->db->query($strQuery);
+		if($this->result==false){
+			return false;
+		}
+		return true;
+	}
+	/*
+	* Fetch from the current data set and return
+	*@return array one record
+	*/
+	function fetch(){
+		//Complete this funtion to fetch from the $this->result
+		if($this->result==null){
+			return false;
+		}
+		
+		if($this->result==false){
+			return false;
+		}
+		
+		return $this->result->fetch_assoc();
+	}
+}
+/*
+This is a test code
+$obj=new adb();
+if(!$obj->query("select * from users"))
+{
+	echo "error";
+	exit();
+}
+print_r($obj->fetch());
+*/
+/*
+$obj = new adb();
+$obj->connect();
+if($obj->connect()==false)
+	echo "could not connect";
+else
+	echo "successful connection";
+
+
+$obj= new adb();
+$result=$obj->query("select * from applications");
+if($result==false)
+	echo "no result obtained";
+else 
+	$obj->fetch();
+
+print_r($obj->fetch());
+*/
+?>
